10c96d8a9b95b558576e57facaf768e9b1ca25ce
oasifsadik <oasifsadik99@gmail.com>
2026-04-20 22:47:34 +0600

    templating

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from '../pages/Login.vue';
+import Home from '../components/Home.vue';
 
 const routes = [
     {
         path: '/login',
         name: 'Login',
         component: Login,
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
     }
 ];
 
